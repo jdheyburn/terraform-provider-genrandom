dadb42e439465127237d503ff50404310f8e5180
Joseph Heyburn <34041368+jdheyburn@users.noreply.github.com>
2020-07-27 19:04:07 +0100

    Fix build

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jdheyburn/terraform-provider-genrandom
+
+go 1.13
+
+require github.com/hashicorp/terraform v0.12.29
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/jdheyburn/terraform-provider-template/genrandom"
+	"github.com/jdheyburn/terraform-provider-genrandom/genrandom"
 )
 
 func main() {
